31aeeb7de54c5873e9e93f195c29ac0a078ee6df
George Xypolitos <gxypol@gmail.com>
2018-12-08 11:39:35 +0200

    auth

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,7 +10,7 @@ import { User } from '../model/user';
   providedIn: 'root',
 })
 export class AuthService {
-  isLoggedIn = false;
+  isLoggedIn = true;
   redirectUrl: string;
 
   constructor(private http: HttpClient,
